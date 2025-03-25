4bf1075787378aa767937863cfa3c6f3781c58be
Matt Shaeffer <matt@theshaeffers.com>
2025-03-25 10:55:01 -0500

    added coming soon and lightbox effect on project page gallery

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+// Lightbox Functionality
+function openLightbox(imageSrc) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = imageSrc; // Set the source of the expanded image
+    lightbox.classList.add('active'); // Show the lightbox
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.classList.remove('active'); // Hide the lightbox
+}
